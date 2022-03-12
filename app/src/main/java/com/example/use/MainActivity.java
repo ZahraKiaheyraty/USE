2b1a6df5ed3a0b20a9e3bd6134ae5b5150ae0062
work <zahra.kiaheyrati1@gmail.com>
2022-03-12 16:21:04 +0330

    first commit

---
+++ app/src/main/java/com/example/use/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.use;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+    static String MQTTHOST = "tcp://45.149.79.49:1883";
+    static String USERNAME = "CWh8AHEEvNFVpGM24bEN";
+    static String PASSWORD = "admin@hamta";
+    String topicStr = "v1/devices/me/telemetry";
+    MqttAndroidClient client;
+
+    String msg =  "{\"temperature\":admin@hamta}";
+    TextView subText;
+    MqttConnectOptions options;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+        options = new MqttConnectOptions();
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(getBaseContext(), " connect", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(getBaseContext(), "dis connect", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                subText.setText(new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+
+    public void pub(View v) {
+        String topic = topicStr;
+        String message = msg;
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(
+                    String.format( "{\"temperature\":%s}", USERNAME) );
+            String temp = jsonObject.getString("temperature");
+
+            client.publish(topic, message.getBytes(), 1, false);
+        } catch (MqttException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
